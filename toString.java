bca9ec529607aef9216184f747c9684462147438
Elena <elenasidyakina@gmail.com>
2017-05-05 14:38:31 -0700

    Create toString.java

---
+++ toString.java
@@ -0,0 +1,15 @@
+//if I have an array and I just want to print out that array, toString is a perfect method. Arrays.toString(myArray) is the format 
+// and must use import java.util.Arrays;
+
+
+
+import java.util.Arrays;
+
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+         int[] a = {1,5,2,7,3};
+        System.out.println(Arrays.toString(a));
+     }
+}
